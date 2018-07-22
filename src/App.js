5a389c17fcd295a2801bedeb4e7bbdc46d16b741
rttang <rttang@admins-MacBook-Pro.local>
2018-07-22 11:53:01 -0700

    Components

--- src/App.js
+++ src/App.js
@@ -36,7 +36,78 @@ class App extends Component {
           let tempCharArray = this.state.chars;
           this.setState({
               chars: this.shuffle(tempCharArray)
-          })
+          });
       }
+
+      findCharsIndex(id) {
+          for (let i = 0; i < this.state.chars.length; i++) {
+              if (parseInt(id, 0) === parseInt(this.state.chars[i].id, 0)) {
+                return i;
+          }
+        }
+      }
+
+      setCharsClicked(index, value) {
+          let tempCharArray = this.state.cats;
+          tempCharArray[index].clicked = value;
+          console.log("Set Chars at index " + index + "clicked to true");
+          this.shuffleChars();
+      }
+
+      resetChars() {
+          let tempCharArray = this.state.chars;
+          tempCharArray = forEach(function(char) {
+              char.clicked = false;
+          });
+          this.shuffleChars();
+          console.log("All Characters Reset");
+      }
+
+      increaseScore() {
+          let tempScore = this.state.score;
+          let tempTopScore = this.state.topScore;
+          tempScore += 1;
+
+          (tempScore >= tempTopScore) ? this.setState({score: tempScore, topScore: tempScore}) : this.setState({ score: tempScore});
+      }
+
+      endGame() {
+          this.setState({
+              score: 0
+          })
+          this.resetCats();
+      }
+
+      setGameActive(toggle) {
+          this.setState({
+              gameActive: toggle
+          });
+      }
+
+      ClickChar = event => {
+
+        let charIndex = this.findCharsIndex(event.target.id);
+        this.setGameActive(true);
+
+        if (this.state.chars[charIndex].clicked === true) {
+            this.setState({
+                result: "Wrong!"
+            });
+            this.endGame();
+        }
+        else {
+            this.increaseScore();
+            this.setState({
+                result: String("Correct!")
+            });
+            this.setCharsClicked(charIndex, true);
+            console.log(this.state);
+            }
+        
+        this.shuffleChars();
+        console.log(this.state.cats);
+        };
       
+
+
 export default App;
